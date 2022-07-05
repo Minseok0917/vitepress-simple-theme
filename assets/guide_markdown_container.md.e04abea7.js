48f51eaeaff8c6053ee9181f5f488439f3b2bcc0
Minseok0917 <Minseok0917@users.noreply.github.com>
2022-07-05 13:43:44 +0000

    Auto deploy from Github Actions

---
+++ assets/guide_markdown_container.md.e04abea7.js
@@ -0,0 +1,46 @@
+import{_ as s,c as n,o as a,a as l}from"./app.ebe65a29.js";const y=JSON.parse('{"title":"Markdown Container","description":"vitepress-simple-theme Markdown Container","frontmatter":{"title":"Markdown Container","description":"vitepress-simple-theme Markdown Container"},"headers":[{"level":2,"title":"\u{1F436} INFO","slug":"\u{1F436}-info"},{"level":2,"title":"\u{1F43A} TIP","slug":"\u{1F43A}-tip"},{"level":2,"title":"\u{1F431} WARNING","slug":"\u{1F431}-warning"},{"level":2,"title":"\u{1F981} DANGER","slug":"\u{1F981}-danger"},{"level":2,"title":"\u{1F42F} DETAILS","slug":"\u{1F42F}-details"},{"level":2,"title":"\u{1F992} MARKDOWN","slug":"\u{1F992}-markdown"}],"relativePath":"guide/markdown/container.md","lastUpdated":1657028565000}'),e={name:"guide/markdown/container.md"},i=l(`<h2 id="\u{1F436}-info" tabindex="-1">\u{1F436} INFO <a class="header-anchor" href="#\u{1F436}-info" aria-hidden="true">#</a></h2><hr><div class="info custom-block"><p class="custom-block-title">POSTCSS</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p><div class="info custom-block"><p class="custom-block-title">SCSS</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p><div class="info custom-block"><p class="custom-block-title">CSS</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p></div></div></div><h2 id="\u{1F43A}-tip" tabindex="-1">\u{1F43A} TIP <a class="header-anchor" href="#\u{1F43A}-tip" aria-hidden="true">#</a></h2><hr><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p></div><h2 id="\u{1F431}-warning" tabindex="-1">\u{1F431} WARNING <a class="header-anchor" href="#\u{1F431}-warning" aria-hidden="true">#</a></h2><hr><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p></div><h2 id="\u{1F981}-danger" tabindex="-1">\u{1F981} DANGER <a class="header-anchor" href="#\u{1F981}-danger" aria-hidden="true">#</a></h2><hr><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p></div><h2 id="\u{1F42F}-details" tabindex="-1">\u{1F42F} DETAILS <a class="header-anchor" href="#\u{1F42F}-details" aria-hidden="true">#</a></h2><hr><details class="details custom-block"><summary>Details</summary><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p></details><h2 id="\u{1F992}-markdown" tabindex="-1">\u{1F992} MARKDOWN <a class="header-anchor" href="#\u{1F992}-markdown" aria-hidden="true">#</a></h2><hr><div class="language-markdown"><span class="copy"></span><pre><code><span class="line"><span style="color:#676E95;font-style:italic;">&lt;!--------------------------------------------------</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">                                INFO</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"> --------------------------------------------------&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: info POSTCSS</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: info SCSS</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: info CSS</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">&lt;!--------------------------------------------------</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">                                TIP</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"> --------------------------------------------------&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: tip</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">&lt;!--------------------------------------------------</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">                                WARNING</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"> --------------------------------------------------&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: warning</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">&lt;!--------------------------------------------------</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">                                DANGER</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"> --------------------------------------------------&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: danger</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">&lt;!--------------------------------------------------</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;">                                DETAILS</span></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"> --------------------------------------------------&gt;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#A6ACCD;">::: details</span></span>
+<span class="line"><span style="color:#A6ACCD;">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span></span>
+<span class="line"><span style="color:#A6ACCD;">:::</span></span>
+<span class="line"></span></code></pre></div>`,18),t=[i];function p(o,c,r,d,m,u){return a(),n("div",null,t)}var C=s(e,[["render",p]]);export{y as __pageData,C as default};
